c86f07144bf8faf10f710115a81dfc078b5608f2
Pau Fracés <pfraces@gmail.com>
2014-09-19 20:59:14 +0200

    Update list.js

--- list.js
+++ list.js
@@ -3,41 +3,41 @@ define('mu.list', function (require) {
 
   var is = require('mu.is');
 
-  var iterateArray = function (list, fn) {
+  var iterateArray = function (list, func) {
     for (var index = 0, len = list.length; index < len; index++) {
-      var exit = fn(list[index], index);
+      var exit = func(list[index], index);
       if (is.defined(exit)) { return exit; }
     }
   };
   
-  var iterateObject = function (list, fn) {
+  var iterateObject = function (list, func) {
     for (var prop in list) {
-      var exit = fn(list[prop], prop);
+      var exit = func(list[prop], prop);
       if (is.defined(exit)) { return exit; }
     }
   };
   
-  var each = function (list, fn) {
-    if (is.array(list)) { return iterateArray(list, fn); }
-    return iterateObject(list, fn);
+  var each = function (list, func) {
+    if (is.array(list)) { return iterateArray(list, func); }
+    return iterateObject(list, func);
   };
 
-  var map = function (list, fn) {
+  var map = function (list, func) {
     var mapped = is.array(list) ? [] : {};
     
     each(list, function (item, index) {
-      mapped[index] = fn(item, index);
+      mapped[index] = func(item, index);
     });
 
     return mapped;
   };
 
-  var filter = function (list, fn) {
+  var filter = function (list, func) {
     var isArray = is.array(list),
         filtered = isArray ? [] : {};
     
     each(list, function (item, index) {
-      if (!fn(item, index)) { return; }
+      if (!func(item, index)) { return; }
       index = isArray ? filtered.length : index;
       filtered[index] = item;
     });
@@ -45,9 +45,9 @@ define('mu.list', function (require) {
     return filtered;
   };
 
-  var reduce = function (list, acc, fn) {
+  var reduce = function (list, acc, func) {
     each(list, function (item, index) {
-      acc = fn(acc, item, index);
+      acc = func(acc, item, index);
     });
     
     return acc;
