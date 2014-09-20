2f79f98055641f40804e9cd8c8e2a70283dde1b6
Pau Fracés <pfraces@gmail.com>
2014-09-20 21:51:42 +0200

    Update list.js

--- list.js
+++ list.js
@@ -1,29 +1,39 @@
-define('mu.list', function (require) {
+define('mu.list.each', function (require) {
   'use strict';
 
-  var is = require('mu.is');
+  var isDefined = require('mu.is.defined'),
+      isArray   = require('mu.is.array');
 
   var iterateArray = function (list, func) {
     for (var index = 0, len = list.length; index < len; index++) {
       var exit = func(list[index], index);
-      if (is.defined(exit)) { return exit; }
+      if (isDefined(exit)) { return exit; }
     }
   };
   
   var iterateObject = function (list, func) {
     for (var prop in list) {
       var exit = func(list[prop], prop);
-      if (is.defined(exit)) { return exit; }
+      if (isDefined(exit)) { return exit; }
     }
   };
   
   var each = function (list, func) {
-    if (is.array(list)) { return iterateArray(list, func); }
+    if (isArray(list)) { return iterateArray(list, func); }
     return iterateObject(list, func);
   };
+  
+  return each;
+});
+
+define('mu.list.map', function (require) {
+  'use strict';
+
+  var isArray = require('mu.is.array'),
+      each    = require('mu.list.each');
 
   var map = function (list, func) {
-    var mapped = is.array(list) ? [] : {};
+    var mapped = isArray(list) ? [] : {};
     
     each(list, function (item, index) {
       mapped[index] = func(item, index);
@@ -31,20 +41,37 @@ define('mu.list', function (require) {
 
     return mapped;
   };
+  
+  return map;
+});
+
+define('mu.list.filter', function (require) {
+  'use strict';
+
+  var isArray = require('mu.is.array'),
+      each    = require('mu.list.each');
 
   var filter = function (list, func) {
-    var isArray = is.array(list),
-        filtered = isArray ? [] : {};
+    var isArr = isArray(list),
+        filtered = isArr ? [] : {};
     
     each(list, function (item, index) {
       if (!func(item, index)) { return; }
-      index = isArray ? filtered.length : index;
+      index = isArr ? filtered.length : index;
       filtered[index] = item;
     });
     
     return filtered;
   };
+  
+  return filter;
+});
 
+define('mu.list.reduce', function (require) {
+  'use strict';
+  
+  var each = require('mu.list.each');
+  
   var reduce = function (list, acc, func) {
     each(list, function (item, index) {
       acc = func(acc, item, index);
@@ -52,6 +79,15 @@ define('mu.list', function (require) {
     
     return acc;
   };
+  
+  return reduce;
+});
+
+define('mu.list.indexOf', function (require) {
+  'use strict';
+
+  var isArray = require('mu.is.array'),
+      each    = require('mu.list.each');
 
   var indexOfArray = function (list, item) {
     var index = [].indexOf.call(list, item);
@@ -66,18 +102,35 @@ define('mu.list', function (require) {
   };
   
   var indexOf = function (list, item) {
-    if (is.array(list)) { return indexOfArray(list, item); }
+    if (isArray(list)) { return indexOfArray(list, item); }
     return indexOfObject(list, item);
   };
+  
+  return indexOf;
+});
+
+define('mu.list.contains', function (require) {
+  'use strict';
+
+  var isDefined = require('mu.is.defined'),
+      indexOf   = require('mu.list.indexOf');
 
   var contains = function (list, item) {
-    return is.defined(indexOf(list, item));
+    return isDefined(indexOf(list, item));
   };
+  
+  return contains;
+});
+
+define('mu.list.removeAt', function (require) {
+  'use strict';
+
+  var isArray = require('mu.is.array');
 
   var removeAt = function (list, index) {
     var removed;
     
-    if (is.array(list)) {
+    if (isArray(list)) {
       removed = ([].splice.call(list, index, 1))[0];
     } else {
       removed = list[index];
@@ -86,21 +139,35 @@ define('mu.list', function (require) {
     
     return removed;
   };
+  
+  return removeAt;
+});
+
+define('mu.list', function (require) {
+  'use strict';
+
+  var isDefined = require('mu.is.defined'),
+      indexOf   = require('mu.list.indexOf'),
+      removeAt  = require('mu.list.removeAt');
 
   var remove = function (list, item) {
     var index = indexOf(list, item);
-    if (is.defined(index)) { removeAt(list, index); }
+    if (isDefined(index)) { removeAt(list, index); }
     return index;
   };
+});
+  
+define('mu.list', function (require) {
+  'use strict';
 
   return {
-    each: each,
-    map: map,
-    filter: filter,
-    reduce: reduce,
-    indexOf: indexOf,
-    contains: contains,
-    removeAt: removeAt,
-    remove: remove
+    each:     require('mu.list.each'),
+    map:      require('mu.list.map'),
+    filter:   require('mu.list.filter'),
+    reduce:   require('mu.list.reduce'),
+    indexOf:  require('mu.list.indexOf'),
+    contains: require('mu.list.contains'),
+    removeAt: require('mu.list.removeAt'),
+    remove:   require('mu.list.remove')
   };
 });
